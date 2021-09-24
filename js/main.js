36b6cd4921105b900a9434e3fb39b2e7a4e1e768
Tavi Selim <oselim14@gmail.com>
2021-09-24 16:33:38 -0700

    update renderBoard to reflect stone count

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 /*----- constants -----*/
 let scores; //player scores end the end. Based on #of stones in their store at end of game. 
 let stones; // game stones, always 48 of them.
+let players = {
+    '-1': 'Player 2',
+    '1': 'Player 1',
+};
+
 
 /*----- app's state (variables) -----*/
 let board; //single array starting in bottom right up to top right
@@ -10,14 +15,13 @@ let playerHand; // when the player picks up stones from a pocket this will fill;
 
 
 /*----- cached element references -----*/
-const pocketEls = Array.from([...document.querySelectorAll('#board > div')]);
-const btnEls= document.querySelector('button');
-const msgEl = document.querySelector('h1');
-
+const pocketEls = Array.from(document.querySelectorAll('#board > div'));
+const btnEl= document.querySelector('button');
+const msgEl = document.querySelector('h1'); 
 
 /*----- event listeners -----*/
-
-
+btnEL = addEventListener('click', init);
+document.getElementById('board').addEventListener('click', playerTurn);
 /*----- functions -----*/
 // intialize the game
 init();
@@ -25,13 +29,48 @@ init();
 //initialize all state var then call render;
 function init() {
   //board for the game when starting
-  board = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]; //each game starts with 4 in each pocket;
-  winner = null;
-  turn= 1; //player one starts, bottom of screen
-  render();
+    board = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]; //each game starts with 4 in each pocket;
+    winner = null;
+    turn= 1; //player one starts, bottom of screen
+    playerHand = 0;
+    render();
 }
 
 function render(){
-  renderBoard();
-  renderMsg();
+    // renderBoard(); // edit images in divs to represent mancala stones. 
+    renderMsg();
+    board.forEach(function(numStones, idx) {
+        pocketEls[idx].innerHTML = numStones;
+    });
+}
+
+function playerTurn(evt){
+    
+    let idx = pocketEls.indexOf(evt, target);
+    if (idx !== null) {
+        playerHand === board[idx];
+        console.log(playerHand);
+    }
+
+    // playerHand = pocketEls[idx];
+    // turn = getTurn();
+    // winner = getWinner();
+    
+}
+
+
+function renderMsg(){
+    if (winner === 1) {
+        msgEl.innerHTML = "Player 1 has won!";
+    } else {
+        msgEl.innerHTML = "Player 2 has won!";
+    }
+}
+
+function getWinner() {
+    if (pockets === 0 && p6 > p13) {
+        return winner = -1;
+    } else if (pockets === 0 && p13 > p6){
+        return winner = 1;
+    }
 }
