71f1e1cc5e09fa9d5ede7b084706e7d70f66ecde
Tavi Selim <oselim14@gmail.com>
2021-09-28 16:04:54 -0700

    board will show legal plays

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 /*----- constants -----*/
-let scores; //player scores end the end. Based on #of stones in their store at end of game. 
-let stones; // game stones, always 48 of them.
 let players = {
     '-1': 'Player 2',
     '1': 'Player 1',
@@ -39,7 +37,14 @@ function render(){
     renderMsg();
     board.forEach(function(numStone, idx) {
         pocketEls[idx].innerHTML = numStone;
+        pocketEls[idx].style.backgroundColor = isIdxInTurn(idx) ? '#fce0cd' : '#DDBEA9';
     });
+}
+
+function isIdxInTurn(idx) {
+    if (turn === -1 && [7,8,9,10,11,12].includes(idx)) return true; 
+    if (turn === 1 && [0,1,2,3,4,5].includes(idx)) return true; 
+    return false;
 }
 
 
@@ -51,38 +56,37 @@ function playerTurn(evt){
     if (idx === 6 || idx === 13 || numStone === 0) return;
     board[idx] = 0;
     while (numStone >= 1){
-        idx = findIndex(idx);
+        idx = getNextIdx(idx);
         board[idx]++; 
         numStone--;
     }
     getTurn(idx);
     render();
-    getWinner(idx);
-
+    getWinner();
+    renderMsg();
+    console.log(winner);
 }
 
-function findIndex(idx) {
+function getNextIdx(idx) {
     idx += 1;
     idx = idx % 14;
-    while (turn === -1 && idx === 6) {
-        (idx += 1);
-        continue;
-    }
-    while (turn === 1 && idx === 13) {
-        (idx = 0);
-        continue;
-    }
+    if (turn === -1 && idx === 6) return idx + 1;
+    if (turn === 1 && idx === 13) return 0;   
     return idx;
 }    
 
 function getTurn(idx) {
     if ((turn === -1 && idx === 13) || (turn === 1 && idx === 6)){
         turn *= 1;
+    } else if ([0,1,2,3,4,5] === 0 || [7,8,9,10,11,12] === 0){
+        turn *= -1;
     } else {turn *= -1};
 }
 
 function renderMsg(){
-    if (winner) {
+    if (winner === 't'){
+        msgEl.innerHTML = `It's a tie!`;
+    } else if (winner) {
         msgEl.innerHTML = `${players[winner]} Wins!`;
     } else {
         msgEl.innerHTML = `${players[turn]}'s Turn!`;
@@ -90,11 +94,17 @@ function renderMsg(){
 }
 
 
-function getWinner(idx) {
-    if ((idx[0] === 0 && idx[1] === 0 && idx[2] === 0 && idx[3] === 0 && idx[4] === 0 && idx[5] === 0) || (idx[7] === 0 && idx[8] === 0 && idx[9] === 0 && idx[10] === 0 && idx[11] === 0 && idx[12] === 0) && getElementByID(p6) > getElementByID(p13)) {
-        return winner = -1;
-    } else if ((idx[0] === 0 && idx[1] === 0 && idx[2] === 0 && idx[3] === 0 && idx[4] === 0 && idx[5] === 0) || (idx[7] === 0 && idx[8] === 0 && idx[9] === 0 && idx[10] === 0 && idx[11] === 0 && idx[12] === 0) && getElementByID(p13) > getElementByID(p6)){
+function getWinner() {
+    if ([0,1,2,3,4,5] === 0 || [7,8,9,10,11,12] === 0 && ([6] > [13])) {
         return winner = 1;
-    }
-    renderMsg();
+    } else if ([0,1,2,3,4,5] === 0 || [7,8,9,10,11,12] === 0 && ([13] > [6])){
+        return winner = -1;
+    } else if ([0,1,2,3,4,5] === 0 || [7,8,9,10,11,12] === 0 && ([13] === [6])){
+        return winner === 't';
+    } else return;
+    // if ((board[0,1,2,3,4,5] === 0 || board[7,8,9,10,11,12] === 0) && (board[13] > board[6])){
+    //     return winner = -1;
+    // } else if ((board[0,1,2,3,4,5]=== 0 || board[7,8,9,10,11,12] === 0) && (board[6] > board[13])){
+    //     return winner = 1;
+    // }
 }
